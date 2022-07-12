727ac48476741dbbe3464b90fba9402b7fe8bac1
Aravinthk95 <107410327+Aravinthk95@users.noreply.github.com>
2022-07-12 16:37:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,91 @@
+// class candidateDetails {
+//     constructor(firstName, lastName, age, dob, department, place, fees) {
+//         this.firstName = firstName;
+//         this.lastName = lastName;
+//         this.age = age;
+//         this.dob = dob;
+//         this.department = department;
+//         this.place = place;
+//         this.fees = fees;
+//     }
+//     getDetails(){
+//     return (`${this.firstName} ${this.lastName} ${this.age} ${this.dob} ${this.department} ${this.place} ${this.fees}`)
+//     }
+// }
+// let candidateDetails1 = new candidateDetails ("Ram", "Ramachandran", "19", "07sep1996", "Petrochemical", "Rawmeswaram", "400000");
+// let candidateDetails2 = new candidateDetails ( "karthi", "moorthi", "18", "32jun1998", "chemical", "karur", "584585");
+// let candidateDetails3 = new candidateDetails ( "David", "Balu", "20", "8Aug1997", "Marine", "coimbatore", "455874");
+
+
+// console.log(candidateDetails1.getDetails());
+// console.log(candidateDetails2.getDetails());
+// console.log(candidateDetails3.getDetails());
+
+
+
+// class ola {
+//     constructor(kilometer, rate, waitingtiming, numberofpersons, luggageperkilogram){
+//         this.kilometer = kilometer;
+//         this.rate = rate;
+//         this.waitingtiming = waitingtiming;
+//         this.numberofpersons = numberofpersons;
+//           this.luggageperkilogram= luggageperkilogram;
+//     }
+//     setKilometer(kilo){
+//         this.kilometer = kilo;
+
+//     }
+//     getRideDetails(){
+//         return (`${this.kilometer} ${this.rate} ${this.waitingtiming} ${this.numberofpersons} ${this.luggageperkilogram}`)
+
+//     }
+//     getPrice(){
+//         let totalPrice = (this.kilometer * this.rate) + (this.waitingtiming * 55) + (this.numberofpersons * 47) + ((this.luggageperkilogram*45) * this.kilometer);
+//         return totalPrice;
+//     }
+// }
+// let olacab1 = new ola (512, 54, 19, 54, 50);
+// let olacab2 = new ola (44425, 57, 95, 89, 47);
+// let olacab3 = new ola ( 548555, 875,255,425, 23);
+
+// console.log(olacab1.getPrice());
+// console.log(olacab2.getPrice());
+// console.log(olacab3.getPrice());
+
+
+
+
+// class movie {
+//     constructor(title, studio, rating){
+//         this.title = title;
+//         this.studio = studio;
+//         this.rating = rating;
+
+//     }
+//     settitle(tit){
+//         this.title = tit;
+//     }
+//     getmovdetails(){
+//      return(`${this.title} ${this.studio} ${this.rating}`)
+    
+//     }
+//     getreview(){
+//         let rev = ((this.title) (this.studio) (this.rating));
+//         return rev;
+//     }
+
+// }
+// let movie1 = new movie ("CasinoRoyale", "EonProduction", "PG13");
+
+// console.log(movie1.getreview());
+
+
+
+class diagram {
+    constructor(radius, colour) {
+        this.radius = radius;
+        this.colour = diagram;
+
+    }
+    
+}
